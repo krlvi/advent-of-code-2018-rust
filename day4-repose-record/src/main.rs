00db6bc22e93d42b409be21a3690c8960d1b2d0a
Kiril Videlov <kiril.vi@runbox.no>
2019-05-22 00:16:26 +0200

    Solve day 4 part 1

--- day4-repose-record/src/main.rs
+++ day4-repose-record/src/main.rs
@@ -1,4 +1,6 @@
 use chrono::NaiveDateTime;
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -10,9 +12,66 @@ fn main() {
 
 fn part_one(file: File) {
     let entries = parse_entries(file);
+    let mut guard_stats: HashMap<usize, HashMap<usize, usize>> = HashMap::new();
+    let guards: HashSet<usize> = entries.iter().map(|x| x.guard_id).collect();
+    guards.iter().for_each(|x| {
+        guard_stats.insert(*x, HashMap::new());
+    });
+    let mut sleep_start: Option<usize> = None;
     for e in entries {
-        println!("{:?}", e);
+        let mut mins = guard_stats
+            .get(&e.guard_id)
+            .expect("No mins entry for guard_id")
+            .clone();
+        let this_min = e
+            .time
+            .time()
+            .format("%M")
+            .to_string()
+            .parse::<usize>()
+            .expect("could not parse second");
+
+        match sleep_start {
+            Some(s) => {
+                if e.awake {
+                    for i in s..this_min {
+                        match mins.get(&i) {
+                            Some(v) => {
+                                mins.insert(i, v + 1);
+                            }
+                            None => {
+                                mins.insert(i, 1);
+                            }
+                        }
+                    }
+                }
+            }
+            None => {}
+        }
+        sleep_start = Some(this_min);
+        guard_stats.insert(e.guard_id, mins);
     }
+
+    let mut totals: Vec<(&usize, usize)> = guard_stats
+        .iter()
+        .map(|(k, v)| (k, v.iter().map(|(_, vv)| *vv).fold(0, |acc, x| acc + x)))
+        .collect();
+    totals.sort_by(|(_, xv), (_, yv)| yv.cmp(xv));
+    let (sleepiest_guard, _) = totals.first().expect("Couldnt get value");
+    println!("{:?}", guard_stats.get(&sleepiest_guard));
+
+    let mut stats: Vec<(&usize, &usize)> = guard_stats
+        .get(sleepiest_guard)
+        .expect("Couldnt find guard stats")
+        .iter()
+        .map(|(k, v)| (k, v))
+        .collect();
+    stats.sort_by(|(_, xv), (_, yv)| yv.cmp(xv));
+
+    let (sleepiest_min, _) = stats.first().expect("Couldnt get value");
+
+    println!("Guard is {} Min is {}", sleepiest_guard, sleepiest_min);
+    println!("Result is {}", **sleepiest_guard * **sleepiest_min);
 }
 
 fn parse_entries(file: File) -> Vec<LogEntry> {
